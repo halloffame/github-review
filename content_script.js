59d8bed141aad0858f7614cf5e8e1013a900266b
Ryan Hall <ryan.hall@biola.edu>
2015-04-28 11:04:18 -0700

    Updating button class to match github
    they changed `octicon-button` to `octicon-btn`

--- content_script.js
+++ content_script.js
@@ -1,5 +1,5 @@
 $('.file.js-details-container > .file-header').each(function() {
-  var new_review_button = $('<a href="#" class="mark_as_reviewed octicon-button tooltipped tooltipped-nw" rel="nofollow" data-skip-pjax="" aria-label="Mark as reviewed"><span class="octicon octicon-check"></span></a>');
+  var new_review_button = $('<a href="#" class="mark_as_reviewed octicon-btn tooltipped tooltipped-nw" rel="nofollow" data-skip-pjax="" aria-label="Mark as reviewed"><span class="octicon octicon-check"></span></a>');
   $(this).find(".file-actions").append(new_review_button);
 })
 
