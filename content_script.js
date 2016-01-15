f35f6668303c31da3da3de8480458c6aa1456399
Ryan Hall <ryan.hall@biola.edu>
2016-01-15 11:01:02 -0800

    Updated to match github update

--- content_script.js
+++ content_script.js
@@ -1,10 +1,11 @@
-$('.file.js-details-container > .file-header').each(function() {
-  var new_review_button = $('<a href="#" class="mark_as_reviewed octicon-btn tooltipped tooltipped-nw" rel="nofollow" data-skip-pjax="" aria-label="Mark as reviewed"><span class="octicon octicon-check"></span></a>');
-  $(this).find(".file-actions").append(new_review_button);
-})
-
-$('.mark_as_reviewed').click(function(){
+var markAsReviewed = function () {
   this.blur(); // Remove focus from link
   $(this).parents('.file.js-details-container').toggleClass('reviewed');
   return false;
+}
+
+$('.file.js-details-container > .file-header').each(function() {
+  var new_review_button = $('<button type="button" class="btn-link btn-octicon tooltipped tooltipped-nw" aria-label="Mark as reviewed"><span aria-hidden="true" class="octicon octicon-check"></span></button>');
+  $(this).find(".file-actions").append(new_review_button);
+  new_review_button.click(markAsReviewed);
 })
